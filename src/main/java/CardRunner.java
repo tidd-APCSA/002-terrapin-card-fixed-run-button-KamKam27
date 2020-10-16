fe8f24354a1d547f47a7f648be4ef284d31e846c
KaMeron Hopkins <620508@aacps.org>
2020-10-16 14:38:48 +0000

    I added all the new excercises

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,24 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
+       // System.out.println("hey");
+      TerrapinCard cardBilly = new TerrapinCard(20);
+      TerrapinCard cardBrian = new TerrapinCard(30);
 
+      cardBilly.payGourmet();
+      cardBrian.payEconomical();
+      System.out.println(cardBilly);
+      System.out.println(cardBrian);
+
+      cardBilly.loadMoney(20);
+      cardBrian.payGourmet();
+      System.out.println(cardBilly);
+      System.out.println(cardBrian);
+
+      cardBilly.payEconomical();
+      cardBilly.payEconomical();
+      cardBrian.loadMoney(50);
+      System.out.println(cardBilly);
+      System.out.println(cardBrian);
     }
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,36 @@
 public class TerrapinCard {
+private double balance;
 
+public TerrapinCard(double StartingBalance) {
+  this.balance = StartingBalance;
+}
+public void payEconomical() {
+ if(balance <= 1){
+   balance = balance;
+} else {
+  balance -= 2.50;
+}
+}
 
+public void payGourmet() {
+ if(balance <= 1){
+   balance = balance;
+} else {
+  balance -= 4.00;
+}
+}
+
+public void loadMoney(double amount) {
+if(balance + amount > 150){
+  balance = 150;
+}else if(balance + amount < 0){
+  balance = balance;
+}else {
+  balance += amount;
+}
+}
+
+public String toString() {
+  return "The card has " + balance;
+}
 }
