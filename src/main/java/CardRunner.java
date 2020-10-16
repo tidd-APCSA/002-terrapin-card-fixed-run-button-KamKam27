6194a415a0e6b3cc468aa9fc4f4aa09d0b2c0845
KaMeron Hopkins <620508@aacps.org>
2020-10-16 14:41:21 +0000

    GG

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -2,23 +2,6 @@ public class CardRunner {
     public static void main(String args[]){
         // ☕
        // System.out.println("hey");
-      TerrapinCard cardBilly = new TerrapinCard(20);
-      TerrapinCard cardBrian = new TerrapinCard(30);
 
-      cardBilly.payGourmet();
-      cardBrian.payEconomical();
-      System.out.println(cardBilly);
-      System.out.println(cardBrian);
-
-      cardBilly.loadMoney(20);
-      cardBrian.payGourmet();
-      System.out.println(cardBilly);
-      System.out.println(cardBrian);
-
-      cardBilly.payEconomical();
-      cardBilly.payEconomical();
-      cardBrian.loadMoney(50);
-      System.out.println(cardBilly);
-      System.out.println(cardBrian);
     }
 }
